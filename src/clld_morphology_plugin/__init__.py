e148f8f131e99ff56a55ed301b4b4c10ad16a78a
Florian Matter <florianmatter@gmail.com>
2022-05-03 14:49:56 +0200

    release 0.0.1

--- src/clld_morphology_plugin/__init__.py
+++ src/clld_morphology_plugin/__init__.py
@@ -4,7 +4,7 @@ from clld_morphology_plugin import models
 
 __author__ = "Florian Matter"
 __email__ = "florianmatter@gmail.com"
-__version__ = "0.0.1.dev"
+__version__ = "0.0.1"
 
 
 def includeme(config):
