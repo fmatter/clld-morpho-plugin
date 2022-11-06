36f6ee1f723a996aa10f8b8aceda213cc13f77e1
Florian Matter <florianmatter@gmail.com>
2022-11-05 20:59:12 -0700

    bump

--- src/clld_morphology_plugin/__init__.py
+++ src/clld_morphology_plugin/__init__.py
@@ -5,7 +5,7 @@ from clld_morphology_plugin import models
 
 __author__ = "Florian Matter"
 __email__ = "florianmatter@gmail.com"
-__version__ = "0.0.6"
+__version__ = "0.0.7.dev"
 
 
 def includeme(config):
