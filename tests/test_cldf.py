d1831ff17328c2cec20d71ee4d411682d715653d
Florian Matter <florianmatter@gmail.com>
2023-02-22 16:46:41 -0800

    rm cldf test

--- tests/test_cldf.py
+++
@@ -1,4 +0,0 @@
-def test_cldf():
-    from clld_morphology_plugin.cldf import FormSlices
-    from clld_morphology_plugin.cldf import MorphsetTable
-    from clld_morphology_plugin.cldf import MorphTable
